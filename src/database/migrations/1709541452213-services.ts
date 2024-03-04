d59cf5a2eca573ea6a4a9c7fc064366df1f7ffd7
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-04 18:20:54 +0100

    migrations complete p-2

--- src/database/migrations/1709541452213-services.ts
+++ src/database/migrations/1709541452213-services.ts
@@ -1,11 +1,35 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Services1709541452213 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "services",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "service_name",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "description",
+            type: "text",
+          },
+        ],
+      }),
+      true
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+    await queryRunner.dropTable("services")
+  }
 }
--- src/database/migrations/1709541514119-appointments.ts
+++ src/database/migrations/1709541514119-appointments.ts
@@ -1,11 +1,60 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableUnique } from "typeorm";
 
 export class Appointments1709541514119 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+              name: "appointments",
+              columns: [
+                {
+                  name: "id",
+                  type: "int",
+                  isPrimary: true,
+                  isGenerated: true,
+                  generationStrategy: "increment",
+                },
+                {
+                  name: "name",
+                  type: "varchar",
+                  length: "40",
+                },
+                {
+                    name: "user_id",
+                    type: "int",
+                  },
+                  {
+                    name: "service_id",
+                    type: "int",
+                  },
+              ],
+              foreignKeys: [
+                {
+                  columnNames: ["user_id"],
+                  referencedTableName: "users",
+                  referencedColumnNames: ["id"],
+                  onDelete: "CASCADE",
+                },
+                {
+                  columnNames: ["service_id"],
+                  referencedTableName: "services",
+                  referencedColumnNames: ["id"],
+                  onDelete: "CASCADE",
+                },
+              ],
+              uniques: [
+                new TableUnique({
+                  name: "user_service_unique",
+                  columnNames: ["user_id", "service_id"],
+                }),
+              ],
+            }),
+            true
+          )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("appointments")
     }
 
 }
